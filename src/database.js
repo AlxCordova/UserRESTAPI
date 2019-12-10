882399e4ed28766ef8cc18e00e979c16d7eaaa39
AlxCordova <agguilar.edwin@gmail.com>
2019-12-10 12:14:03 -0600

    Cluster Atlas connection

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connect() {
-    await mongoose.connect('mongodb://localhost/flutter-node', {
+    await mongoose.connect('mongodb+srv://flutter:flutter123@cluster0-zjtqq.mongodb.net/test?retryWrites=true&w=majority', {
         useNewUrlParser: true,
         // Warning in MongoClient constructor
         useUnifiedTopology: true
